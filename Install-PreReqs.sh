d15824ccbd074ce85b81bf0729ad1ae6ca9989c5
Burhan U Din Arshad <45567298+burhanudinarshad@users.noreply.github.com>
2020-07-05 15:52:37 +0500

    Create Install-PreReqs.sh

---
+++ Install-PreReqs.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+
+#Install AWS CLI
+sudo apt-get update
+sudo apt-get install python-pip
+pip install awscli --upgrade --user
+
+
+#Install KubeCTL
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+#install EKSCTL
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
