06fce14e7522823f846e7668bbbdbef2cff5ff93
Burhan U Din Arshad <45567298+burhanudinarshad@users.noreply.github.com>
2020-07-05 17:04:26 +0500

    Update Install-PreReqs.sh

--- Install-PreReqs.sh
+++ Install-PreReqs.sh
@@ -14,3 +14,9 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 #install EKSCTL
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+
+
+#Install Helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
